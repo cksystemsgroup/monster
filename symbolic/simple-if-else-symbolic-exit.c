3a0b0d88db84e0247745fc231b982d3948ac59af
alexanderlackner <43965864+alexanderlackner@users.noreply.github.com>
2020-09-14 14:19:55 +0200

    Updated comment in newly created c file

--- symbolic/simple-if-else-symbolic-exit.c
+++ symbolic/simple-if-else-symbolic-exit.c
@@ -1,5 +1,15 @@
-//this file does pretty much the same as simple-if-else-1-35.c
-//but has symbolic exit codes since we only support symbolic exits for now
+/*
+The purpose of this code is to demonstrate the capabilities
+of the monster model generator of selfie. Monster translates
+the code to an SMT-LIB or BTOR2 formula that is satisfiable
+if and only if the code exits with a non-zero exit code, or
+performs division by zero or invalid/unsafe memory accesses.
+
+This code is exactly the same as simple-if-else-1-35.c but with symbolic exit codes.
+This is necessary since we only support symbolic exit codes so far.
+
+Input == #b00110001 (== 49 == '1')
+*/
 
 uint64_t main() {
   uint64_t  a;
