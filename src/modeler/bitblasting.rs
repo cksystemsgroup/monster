6768ae3c5f0ff40216431287069a8a83915053db
smml1996 <michelle_muroya@hotmail.com>
2022-01-20 02:38:29 +0100

    feat: bitblasting input, state, not, and, bad

--- src/modeler/bitblasting.rs
+++ src/modeler/bitblasting.rs
@@ -1,4 +1,4 @@
-use crate::modeler::{get_bitsize, HashableNodeRef, Model, Node, NodeRef};
+use crate::modeler::{get_bitsize, HashableNodeRef, Model, Node, NodeRef, NodeType};
 use std::collections::HashMap;
 use std::rc::Rc;
 
@@ -10,20 +10,17 @@ pub fn bitblast_model(model: &Model) -> Vec<GateRef> {
 }
 type GateRef = Rc<Gate>;
 
+#[derive(PartialEq, Eq)]
 pub enum Gate {
     ConstTrue,
     ConstFalse,
-    // InputBit,
-    // Not{
-    //     value: GateRef,
-    // },
+    InputBit,
+    Not { value: GateRef },
+    And { left: GateRef, right: GateRef },
     // Bad {
     //     cond: GateRef,
     // },
-    // And {
-    //     left: GateRef,
-    //     right: GateRef,
-    // },
+
     // Nand {
     //     left: GateRef,
     //     right: GateRef,
@@ -72,10 +69,10 @@ impl BitBlasting {
         }
     }
 
-    fn record_mapping(&mut self, node: &NodeRef, replacement: Vec<GateRef>) {
+    fn record_mapping(&mut self, node: &NodeRef, replacement: Vec<GateRef>) -> Vec<GateRef> {
         let key = HashableNodeRef::from(node.clone());
         assert!(!self.mapping.contains_key(&key));
-        self.mapping.insert(key, replacement);
+        self.mapping.insert(key, replacement).unwrap()
     }
 
     fn query_existence(&mut self, node: &NodeRef) -> Option<Vec<GateRef>> {
@@ -96,21 +93,158 @@ impl BitBlasting {
         }
     }
 
+    fn get_replacement_from_constant(&mut self, sort: &NodeType, value_: u64) -> Vec<GateRef> {
+        let total_bits = get_bitsize(sort);
+        let mut replacement: Vec<GateRef> = Vec::new();
+        let mut value = value_;
+        for _ in 0..total_bits {
+            replacement.push(self.get_gate_from_constant_bit(value % 2));
+            value /= 2;
+        }
+        replacement
+    }
+
+    fn get_replacement_from_unique_gate(
+        &mut self,
+        sort: &NodeType,
+        gate_type: Gate,
+    ) -> Vec<GateRef> {
+        let total_bits = get_bitsize(sort);
+        let mut replacement: Vec<GateRef> = Vec::new();
+        let gate = GateRef::from(gate_type);
+        for _ in 0..total_bits {
+            replacement.push(gate.clone());
+        }
+        replacement
+    }
+
+    fn is_constant(&mut self, gate_type: GateRef) -> bool {
+        *gate_type == Gate::ConstFalse || *gate_type == Gate::ConstTrue
+    }
+
+    fn get_constant(&mut self, gate_type: GateRef) -> Option<bool> {
+        if self.is_constant(gate_type.clone()) {
+            if *gate_type == Gate::ConstFalse {
+                Some(false)
+            } else {
+                Some(true)
+            }
+        } else {
+            None
+        }
+    }
+
+    fn visit(&mut self, node: &NodeRef) -> Vec<GateRef> {
+        let key = HashableNodeRef::from(node.clone());
+        if self.mapping.contains_key(&key) {
+            self.mapping.get(&key).cloned().unwrap()
+        } else {
+            let replacement = self.process(node);
+            assert!(!self.mapping.contains_key(&key));
+            replacement
+        }
+    }
+
     fn process(&mut self, node: &NodeRef) -> Vec<GateRef> {
         match &*node.borrow() {
             Node::Const { nid: _, sort, imm } => {
                 if let Some(replacement) = self.query_existence(node) {
                     replacement
                 } else {
-                    let total_bits = get_bitsize(sort);
-                    let mut replacement: Vec<GateRef> = Vec::new();
-                    let mut value = *imm;
-                    for _ in 0..(total_bits) {
-                        replacement.push(self.get_gate_from_constant_bit(value % 2));
-                        value /= 2;
+                    let replacement = self.get_replacement_from_constant(sort, *imm);
+                    self.record_mapping(node, replacement)
+                }
+            } Node::Input { nid: _, sort, name: _ } => {
+                if let Some(replacement) = self.query_existence(node) {
+                    replacement
+                } else {
+                    let replacement = self.get_replacement_from_unique_gate(sort, Gate::InputBit);
+                    self.record_mapping(node, replacement)
+                }
+
+            } Node::State { nid: _, sort, init, name: _ } => {
+                if let Some(replacement) = self.query_existence(node) {
+                    replacement
+                }else {
+                    let replacement;
+                    if let Some(value) = init {
+                        replacement = self.visit(value);
+                    } else {
+                        replacement = self.get_replacement_from_unique_gate(sort, Gate::ConstFalse);
                     }
-                    self.record_mapping(node, replacement);
-                    self.query_existence(node).unwrap()
+                    self.record_mapping(node, replacement)
+                }
+            }  Node::Not { nid: _, value } => {
+                if let Some(replacement) = self.query_existence(node) {
+                    replacement
+                } else {
+                    let bitvector = self.visit(value);
+                    let mut replacement: Vec<GateRef> = Vec::new();
+
+                    for bit in bitvector {
+                        if self.is_constant(bit.clone()) {
+                            if *bit == Gate::ConstFalse {
+                                replacement.push(GateRef::from(Gate::ConstTrue));
+                            } else {
+                                assert!(*bit == Gate::ConstTrue);
+                                replacement.push(GateRef::from(Gate::ConstFalse));
+                            }
+                        } else {
+                            replacement.push(GateRef::from(Gate::Not{value:bit}))
+                        }
+                    }
+                    self.record_mapping(node, replacement)
+                }
+            }  Node::Bad { nid: _, cond, name: _ } => {
+                if let Some(replacement) = self.query_existence(node) {
+                    replacement
+                } else {
+                    let replacement = self.visit(cond);
+                    self.record_mapping(node, replacement)
+                }
+            } Node::And { nid: _, left, right } => {
+                if let Some(replacement) = self.query_existence(node) {
+                    replacement
+                } else {
+                    let left_operand = self.visit(left);
+                    let right_operand = self.visit(right);
+                    assert!(left_operand.len() == right_operand.len());
+
+                    let mut replacement: Vec<GateRef> = Vec::new();
+                    for (l_bit, r_bit) in left_operand.iter().zip(right_operand.iter()) {
+                        if let Some(l_bit_const) = self.get_constant(l_bit.clone()) {
+                            if let Some(r_bit_const) = self.get_constant(r_bit.clone()) {
+                                if l_bit_const && r_bit_const {
+                                    replacement.push(GateRef::from(Gate::ConstTrue));
+                                } else {
+                                    replacement.push(GateRef::from(Gate::ConstFalse));
+                                }
+                            } else if l_bit_const {
+                                replacement.push((*r_bit).clone());
+                            } else {
+                                replacement.push(GateRef::from(Gate::ConstFalse));
+                            }
+                        } else if let Some(r_bit_const) = self.get_constant(r_bit.clone()) {
+                            if r_bit_const {
+                                replacement.push((*l_bit).clone());
+                            } else {
+                                replacement.push(GateRef::from(Gate::ConstFalse));
+                            }
+                        } else {
+                            replacement.push(GateRef::from(Gate::And{left:(*l_bit).clone(), right:(*r_bit).clone()}));
+                        }
+                    }
+                    self.record_mapping(node, replacement)
+                }
+            } Node::Ext { nid: _, from, value } => {
+                if let Some(replacement) = self.query_existence(node) {
+                    replacement
+                } else {
+                    let mut replacement: Vec<GateRef> = self.visit(value);
+                    while replacement.len() < get_bitsize(from) {
+                        replacement.push(GateRef::from(Gate::ConstFalse));
+                    }
+                    self.record_mapping(node, replacement)
                 }
             }
             _ => {
@@ -132,28 +266,10 @@ impl BitBlasting {
               //     //buffer.write_all(format!("{} urem 2 {} {}\n", nid, get_nid(left), get_nid(right)).as_bytes())?,
               // Node::Ult { nid, left, right } =>
               //     //buffer.write_all(format!("{} ult 1 {} {}\n", nid, get_nid(left), get_nid(right)).as_bytes())?,
-              // Node::Ext { nid, from, value } =>
-              //     //buffer.write_all(format!("{} uext 2 {} {}\n", nid, get_nid(value), 64 - get_bitsize(from)).as_bytes())?,
               // Node::Ite { nid, sort, cond, left, right } =>
               //     buffer.write_all(format!("{} ite {} {} {} {}\n", nid, get_sort(sort), get_nid(cond), get_nid(left), get_nid(right)).as_bytes())?,
               // Node::Eq { nid, left, right } =>
               //     buffer.write_all(format!("{} eq 1 {} {}\n", nid, get_nid(left), get_nid(right)).as_bytes())?,
-              // Node::And { nid, left, right } =>
-              //     buffer.write_all(format!("{} and 1 {} {}\n", nid, get_nid(left), get_nid(right)).as_bytes())?,
-              // Node::Not { nid, value } =>
-              //     buffer.write_all(format!("{} not 1 {}\n", nid, get_nid(value)).as_bytes())?,
-              // Node::State { nid, sort, init, name } => {
-              //     buffer.write_all(format!("{} state {} {}\n", nid, get_sort(sort), name.as_deref().unwrap_or("?")).as_bytes())?;
-              //     if let Some(value) = init {
-              //         buffer.write_all(format!("{} init {} {} {}\n", nid + 1, get_sort(sort), nid, get_nid(value)).as_bytes())?;
-              //     }
-              // }
-              // Node::Next { nid, sort, state, next } =>
-              //     buffer.write_all(format!("{} next {} {} {}\n", nid, get_sort(sort), get_nid(state), get_nid(next)).as_bytes())?,
-              // Node::Input { nid, sort, name } =>
-              //     buffer.write_all(format!("{} input {} {}\n", nid, get_sort(sort), name).as_bytes())?,
-              // Node::Bad { nid, cond, name } =>
-              //     buffer.write_all(format!("{} bad {} {}\n", nid, get_nid(cond), name.as_deref().unwrap_or("?")).as_bytes())?
         }
     }
 
